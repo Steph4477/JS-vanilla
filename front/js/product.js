f6623c2b65ea4188379656dedc22a028a94796bc
Stéphane Morel <110528810+Steph4477@users.noreply.github.com>
2022-12-22 15:50:08 +0100

    Quantité
    - Ajout de la quantité si même id et même couleur présent dans le localStorage.

--- front/js/product.js
+++ front/js/product.js
@@ -64,6 +64,7 @@ function construction(article){
 ///////// EVENEMENT DU BOUTON ET VALIDATION DU TICKET ////////////////////////////////////////
 // Je crée une constante du bouton qui cible l'ID.
 const bouton = document.getElementById("addToCart");
+
 // Je crée une fonction qui écoute les evenements du bouton.
 function alerteColor(){
     let couleur = document.querySelector(".item__content__settings__color");
@@ -71,69 +72,72 @@ function alerteColor(){
     let alerte = document.createElement("h3");
     couleur.appendChild(alert);
     alert.appendChild(alerte);
-    alerte.textContent = "-- Choisissez une couleur !!! --";
-    alerte = true;
+    alerte.textContent = "-- Choisissez une couleur !!! --";   
 }
-
 function alerteQuantite(){
     let couleur = document.querySelector(".item__content__settings__quantity");
     let alert = document.createElement("div")
     let alerte = document.createElement("h3"); 
     couleur.appendChild(alert);
     alert.appendChild(alerte);
-    alerte.textContent = "-- Choisissez une quantité comprise entre 1 et 100 articles maximum !!! --";
-    alerte = true;
+    alerte.textContent = "-- Choisissez une quantité comprise entre 1 et 100 articles maximum !!! --";  
 }
+
 bouton.addEventListener("click", function () {
     // Je crée des variables variable qui récupère les valeurs sélectionné par le visiteur. 
     let quantite = document.getElementById("quantity").value;
+    ///////////////////////////    ALERTES   /////////////////////////////////////////////////
     // Je crée des alertes si les valeurs sont mal ou pas remplis.
     let color = document.querySelector("#colors").value;
-    
-    
     if ((color ==="") && (quantite === "0")){
-        alerteColor();
-        alerteQuantite();
+        alerteColor()
+        alerteQuantite()
     }
     else if (color === "") {
-        alerteColor(); 
+        alerteColor()
     }
-    
-    else if (quantite === "0") {
-        alerteQuantite();
+    else if (quantite === 0) {
+        alerteQuantite()
     }
-   
+    else if ((quantite >= 100) || (quantite <= 0)) {
+        alerteQuantite()
+    }
     let prix = document.getElementById("price").textContent;
     let nom = document.getElementById("title").textContent;
-    
+    // je met les tickets dans le localStorage au format JSON.
+    let local = JSON.parse(localStorage.getItem("tickets"));
     // Je crée le ticket qui sera un objet
     let ticket = {
         name : nom,
         id : id,
-        quantite : quantite,
-        couleur : color,
-        alerte : false
+        quantite : Number(quantite),
+        couleur : color
     }
-    console.log(ticket);
-    // je met les tickets dans le localStorage au format JSON.
-    let local = JSON.parse(localStorage.getItem("tickets"));
-    console.log(local)
     // Je crée des exeptions et les rentrent dans une variable null.
     // Si le local est null ajoute le aux tickets
     if (local === null){
         local = [];
-       
         local.push(ticket);
-       
         localStorage.setItem("tickets", JSON.stringify(local));
+    }
+    else{
+        // Si la variable recherche à le même id et la même couleur qu'un ticket présent.
+        let recherche = local.find(element => element.id == ticket.id && element.couleur == ticket.couleur);
+        // Si pas definit, crée un nouveau ticket.
+        if (recherche == undefined){
+            local.push(ticket);
+            localStorage.setItem("tickets", JSON.stringify(local));
+        }
+        // Sinon rajoute la quantité.
+        else { 
+            recherche.quantite += ticket.quantite;
+            localStorage.setItem("tickets", JSON.stringify(local));
+        }
     }
     // Si le locale est différent de null. 
-    else if (local != null){
-        
+    if (local != null){  
         local.push(ticket);
         localStorage.setItem("tickets", JSON.stringify(local));
-        // findIndex
     }
     console.log(local);
-
 });
